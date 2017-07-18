4019e15047938d1188e039aeb4beba12fa3b4573
taylorlcm <talachmi@gmail.com>
2017-07-18 12:47:02 -0600

    Add files via upload

---
+++ Development Challenge/contents.js
@@ -0,0 +1,28 @@
+var method = [
+{
+    "title" :"ROASTING:",
+    "paragraph" :"<strong>Ovens</strong> aren’t really common in Chinese homes. But that doesn’t mean that Chinese people don’t enjoy a lot of roasted goodies. You can get meat roasted over open flames outdoors, or in large, hung ovens. Peking duck, for instance, is often roasted in hung ovens, which are designed to roast up to 20 ducks simultaneously over an open wood-burning fire. While cooking, the chef can use a long pole to crisp the ducks by moving them closer to the fire.",
+    "paragraph2":"Pictured here is our Braised Roast Duck but roasting is definitely not invented by Chinese and probably the method that is most familiar to western cooks.  if you have a minute, check out our Orange Five Spice Roast Goose we made for Christmas dinner."
+},
+
+{
+    "title" :"STEAMING:",
+    "paragraph" :"Cooking food by placing it over boiling water so that the steam reaches and cooks the food. You can use a stainless steel steamer, a <strong>bamboo</strong> steamer or a metal steam rack sitting inside a wok or pot. Make sure the bowl or the plate used in this method is heat resistant or you’ll open your pot expecting to see perfectly cooked, fluffy pork buns and instead will be greeted with a mess of broken china and soggy, semi-boiled food. Also, pay special attention to the water level, as water shouldn’t touch the food while boiling. At the same time, you don’t want your pot to boil dry. We prefer the stainless steel steamer; it’s always there, sitting on the stove next to the wok."
+
+}
+
+];
+var output = document.getElementById('title');
+output.innerHTML = method[0].title;
+
+var output1 = document.getElementById('pa');
+output1.innerHTML = method[0].paragraph;
+
+var output2 = document.getElementById('pa2');
+output2.innerHTML = method[0].paragraph2;
+
+var output3 = document.getElementById('title1');
+output3.innerHTML = method[1].title;
+
+var output4 = document.getElementById('pa1');
+output4.innerHTML = method[1].paragraph;
